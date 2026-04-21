b0872c55eba530204341725fe86c6cfd40b74367
Daniel Parker <d.parker2761@gmail.com>
2026-04-21 15:44:08 -0600

    finished

--- lab14.cpp
+++ lab14.cpp
@@ -2,18 +2,23 @@
 then find the average.*/
 //Daniel Parker 04/21/2026
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
 
 //function declarations
-void selectionSort(double* testPtr[], int size);
-void swap(int& num1, int& num2);
+void selectionSort(double* testPtr, int size);
+void swap(double& num1, double& num2);
+double average(double* testPtr, int size);
+void arrayOut(double* testPtr, int size);
+void getScores(double* testPtr, int size);
 
 //main function driver for sort and average functions as well as output
 int main()
 {
-	//create pointer and size
-	double* testPtr, size;
+	//create pointer, size and average value
+	double* testPtr, avg;
+	int size;
 
 	//greeting
 	cout << "This program will get your test scores, sort, and average them." << endl;
@@ -25,20 +30,42 @@ int main()
 	//dynamically allocate test score array.
 	testPtr = new double[size];
 
+	//get test scores from user
+	getScores(testPtr, size);
+
 	//function call to sort
+	selectionSort(testPtr, size);
+
+	//get average
+	avg = average(testPtr, size);
+
+	//function call to output
+	arrayOut(testPtr, size);
+
+	//output average
+	cout << "The average score is: " << setprecision(3) << avg << endl;
+
+	return 0;
 }
 
-void selectionSort(double* testPtr[], int size)
+void selectionSort(double* testPtr, int size)
 {
+	//create indexes and smallNum tracker and pointer for smallest number
 	int i, index;
-	double smallNum;
+	double smallNum = 0;
+	int smallIndex;
 
-	for (index = 0; index < size; i++)
+	for (index = 0; index < size - 1; index++)
 	{
-		for (i = 0; i < size - 1; i++)
+		smallNum = testPtr[index];
+		smallIndex = index;
+		for (i = index +1; i < size; i++)
 			if (smallNum > *(testPtr + i))
+			{
 				smallNum = *(testPtr + i);
-		swap()
+				smallIndex =  i;
+			}
+		swap(*(testPtr + smallIndex), *(testPtr + index));
 	}
 }
 
@@ -51,4 +78,35 @@ void swap(double& num1, double& num2)
 	temp = num1;
 	num1 = num2;
 	num2 = temp;
+}
+
+double average(double* testPtr, int size)
+{
+	//create running total and average
+	double runTot = 0, avg;
+	//loop add
+	for (int i = 0; i - 1 < size; i++)
+	{
+		runTot += *(testPtr + i);
+	}
+	//get average
+	avg = runTot / size;
+
+	return avg;
+}
+
+void arrayOut(double* testPtr, int size)
+{
+	for (int i = 0; i < size; i++)
+		cout << "test" << i + 1 << ": " << * (testPtr + i) << endl;
+}
+
+void getScores(double* testPtr, int size)
+{
+	//loop for filling array
+	for (int i = 0; i < size; i++)
+	{
+		cout << "Enter a test score: ";
+		cin >> *(testPtr + i);
+	}
 }
